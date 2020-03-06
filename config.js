e563212f1bf215cb4b84a49c483784d70705f74f
kristinzhanggit <kristinzhangpku@gmail.com>
2020-03-07 00:08:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ var config = {
      */
     imgs: {
         "心爱的小可爱": "./imgs/xiaokeai.png",
-        "心爱的小可爱": "./imgs/tongnian.jpg",
-        "心爱的小可爱": "./imgs/yunnan.jpg",
-        "心爱的小可爱": "./imgs/riben.jpg",
-        "心爱的小可爱": "./imgs/haibian.jpg",
-        "今天是你的生日": "./imgs/birthday.jpg",
+        //"心爱的小可爱": "./imgs/tongnian.jpg",
+        //"心爱的小可爱": "./imgs/yunnan.jpg",
+        //"心爱的小可爱": "./imgs/riben.jpg",
+        //"心爱的小可爱": "./imgs/haibian.jpg",
+        //"今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
